7dbde695faf482c033943a55bc1a2a77bc50ea5b
jrlepere <jrlepere1@gmail.com>
2018-11-14 21:38:28 -0800

    movie info extractor init

---
+++ src/main/java/MovieInformationExtractor.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class MovieInformationExtractor {
+
+	private Document movieDocument;
+	
+	public MovieInformationExtractor(String rottenTomatoesURL) throws IOException {
+		this.movieDocument = Jsoup.connect(rottenTomatoesURL).get();
+	}
+	
+	public String getMovieDescription() {
+		return this.movieDocument.select("meta[property=og:description]").get(0).attr("content");
+	}
+	
+	public String getMovieTitle() {
+		return this.movieDocument.select("meta[property=og:title]").get(0).attr("content");
+	}
+	
+}
---
+++ src/test/java/MovieInformationExtractorTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class MovieInformationExtractorTest {
+
+	private MovieInformationExtractor movieInformationExtractor;
+	
+	@Before
+	public void loadMovieInformationExtractor() throws IOException {
+		this.movieInformationExtractor = new MovieInformationExtractor(
+				"https://www.rottentomatoes.com/m/the_ballad_of_buster_scruggs");
+	}
+	
+	@Test
+	public void getMovieDescriptionTest() throws IOException { 
+		String movieDescriptionExpected = "The Ballad of Buster Scruggs is a six-part "
+				+ "Western anthology film, a series of tales about the American frontier "
+				+ "told through the unique and incomparable voice of Joel and Ethan Coen. "
+				+ "Each chapter tells a distinct story about the American West.";
+		String movieDescriptionActual = movieInformationExtractor.getMovieDescription();
+		assertEquals(movieDescriptionExpected, movieDescriptionActual);
+	}
+	
+	@Test
+	public void getMovieTitleTest() throws IOException { 
+		String movieTitleExpected = "The Ballad of Buster Scruggs";
+		String movieTitleActual = movieInformationExtractor.getMovieTitle();
+		assertEquals(movieTitleExpected, movieTitleActual);
+	}
+	
+}
